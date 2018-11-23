9a88fae2b5c9b03148769fe5173cb780eed1529c
Jaime Garcia Luna <jluna@alpha10.aulas.gsyc.urjc.es>
2018-11-23 17:52:37 +0100

    casi terminado client.py

--- client.py
+++ client.py
@@ -16,36 +16,39 @@ if len(sys.argv)!=3:
 SERVER = 'localhost'
 PORT = 6001
 
+try:
+	# Contenido que vamos a enviar
+	LINE = '¡Hola mundo!'
 
-# Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
+	METHOD = sys.argv[1]
+	DIR = sys.argv[2]
+	LOGIN = DIR.split('@')[0] 
+	IP = DIR.split(':')[0].split('@')[1]
+	PORT = int(DIR.split(':')[1])
+except:
+	sys.exit('Usage: python client.py method reciever@IP:SIPport')
 
-SERVER = 'localhost'
-METHOD = sys.argv[1]
-DIR = sys.argv[2]
-LOGIN = DIR.split('@')[0] 
-IP = DIR.split(':')[0].split('@')[1]
-PORT = int(DIR.split(':')[1])
-
-
-#Enviamos:
-LINE = METHOD + 'sip:' + DIR + '@' + IP + 'SIP/2.0\r\n\r\n'
-
-
+	#Enviamos:
+LINE = METHOD + ' sip:' + DIR + '@' + IP + 'SIP/2.0\r\n\r\n'
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
-    my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    my_socket.connect((IP, PORT))
+	my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	my_socket.connect((IP, PORT))
 
-    print("Enviando: " + LINE)
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
-    data = my_socket.recv(1024)
+	print("Enviando: " + LINE)
+	my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+	data = my_socket.recv(1024)
 
 #Envio el ACK en caso de recibir el TRYING, RING y OK
-rec_invite = data.decode('utf-8').split('\r\n\r\n')[0:-1]
-    
-print('Recibido -- ', data.decode('utf-8'))
+message = data.decode('utf-8')
+if message == ['SIP/2.0 100 Trying', 'SIP/2.0 180 Ring','SIP/2.0 200 Ok']:
+	ms_ack = 'ACK' + DIR + '@' + IP + 'SIP/2.0\r\n\r\n'
+	print("Enviamos: " + ms_ack)
+	my_socket.send(bytes(ms_ack, 'utf-8') + b'\r\n\r\n')
+if message == b"SIP/2.0 200 OK \r\n\r\n":
+	print("ACK recibido")
+	
 print("Terminando socket...")
 
 print("Fin.")
